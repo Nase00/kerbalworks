4b07261269b01c305f06b41ff69c4e0cf5380974
Apprentice <nase00@gmail.com>
2015-01-13 09:53:21 -0600

    form partials fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    respond_to do |format|
-        if user and user.authenticate(params[:session][:password])
-          session[:user_id] = user.id
-          format.html { redirect_to root_path }
-          format.json { head :no_content }
-          format.js
-        else
-          format.html { redirect_to login_url, alert: "Wrong login or password!" }
-          format.json {render json: "Wrong login or password", status: :unprocessable_entity}
-          format.js
-      end
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render 'new'
     end
   end
 
   def destroy
-    log_out if logged_in?
-    redirect_to root_url
+    session[:user_id] = nil
+    redirect_to root_path
   end
-
- 	def log_out
-		session[:user_id] = nil
-	end
 end
