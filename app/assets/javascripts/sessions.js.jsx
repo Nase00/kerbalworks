e389b506813ed77cbc549338c7b97ae534ed5141
nase00 <nase00@gmail.com>
2015-02-02 00:40:34 -0600

    Forming login box.

--- app/assets/javascripts/sessions.js.jsx
+++ app/assets/javascripts/sessions.js.jsx
@@ -1,12 +1,27 @@
 domready(function () {
   var LoginBox = React.createClass({
 	  render: function() {
-	    return <div>Hello {this.props.name}</div>;
+	    return (
+	    	<section id='login-box'>
+					<h2>Login</h2>
+					<form id='login-form' action='/login' method='POST'>
+						<div className='fields'>
+							<label for='login-form-email'>Email</label>
+							<input id='login-form-email' type='text' name='session[email]' placeholder='Email' />
+							<input type='password' name='session[password]' placeholder='Password' />
+						</div>
+					  <div className='buttons'>
+					  	<input id='submit-login' className='affirm' type='submit' />
+					  </div>
+					</form>
+				</section>
+		  );
 	  }
 	});
 	console.log('test1')
-	document.getElementById('session').onclick=function whatevs() {
+	document.getElementById('login-link').onclick=function showLogin(e) {
+		e.preventDefault()
 		console.log('test2')
-		React.render(<LoginBox name="Sean" />, document.getElementById('session'));
+		React.render(<LoginBox />, document.getElementById('login-anchor'));
 	}
 })
