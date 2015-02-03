5bb8a6c14a50c9258fa7f4a1685bc88645a0ae25
nase00 <nase00@gmail.com>
2015-02-02 18:42:53 -0600

    Meh.

--- app/assets/javascripts/sessions.js.jsx
+++ app/assets/javascripts/sessions.js.jsx
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+	var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
 	var LoginLink = React.createClass({
   	getInitialState: function() {
       return { shownForm: false };
@@ -10,7 +12,6 @@ $(document).ready(function () {
 				this.setState({ shownForm: false })
 			} else {
 				React.render(<LoginForm />, document.getElementById('login-anchor'));
-				this.setState({ shownForm: true })
 			}
 		},
 		render: function() {
@@ -21,7 +22,7 @@ $(document).ready(function () {
   var LoginForm = React.createClass({
 	  handleSubmit: function(e) {
 			e.preventDefault();
-			this.setState({ showLoginForm: false });
+			React.render(<span />, document.getElementById('login-anchor'));
 			var request = $.ajax({
 		    url: "/login",
 		    method: "post",
@@ -34,18 +35,19 @@ $(document).ready(function () {
 			return;  	
 	  },
 	  render: function() {
+	  	LoginLink.setState({ shownForm: true })
 	    return (
-				<form className='loginForm' onSubmit={this.handleSubmit}>
-					<div className='fields'>
-						<label>Email</label>
-						<input type='text' name='session[email]' placeholder='Email' />
-						<label>Password</label>
-						<input type='password' name='session[password]' placeholder='Password' />
-					</div>
-				  <div className='buttons'>
-				  	<input className='affirm' type='submit' />
-				  </div>
-				</form>
+	    	<section className='session-form'>
+					<form onSubmit={this.handleSubmit}>
+						<div className='fields'>
+							<input type='text' name='session[email]' placeholder='Email' />
+							<input type='password' name='session[password]' placeholder='Password' />
+						</div>
+					  <div className='buttons'>
+					  	<input className='affirm' type='submit' />
+					  </div>
+					</form>
+				</section>
 		  );
 	  }
 	});
